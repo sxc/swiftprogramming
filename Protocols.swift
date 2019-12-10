f8d05139ffabcdf52a44310a4bc09730d5b07224
Shen Xiaochun <3212762+sxc@users.noreply.github.com>
2019-12-10 10:51:09 +0800

    Update Protocols.swift

--- Protocols.swift
+++ Protocols.swift
@@ -1,12 +1,24 @@
 import UIKit
 
-func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
+protocol TabularDataSource {
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+    
+    func label(forColumn column: Int) -> String
+
+    func itemFor(row: Int, column: Int) -> String
+}
+
+//func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
+func printTable(_ dataSource: TabularDataSource) {
     var firstRow = "|"
     
     
     var columnWidths = [Int]()
     
-    for columnLabel in columnLabels {
+//    for columnLabel in columnLabels {
+    for i in 0 ..< dataSource.numberOfColumns {
+        let columnLabel = dataSource.label(forColumn: i)
         let columnHeader = "\(columnLabel) |"
         firstRow += columnHeader
         columnWidths.append(columnLabel.count)
@@ -14,7 +26,8 @@ func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
     }
     print(firstRow)
     
-    for row in data {
+//    for row in data {
+    for i in 0 ..< dataSource.numberOfRows {
         var out = "|"
         
 //        for item in row {
@@ -22,7 +35,9 @@ func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
 //        }
 //
         
-        for (j, item) in row.enumerated() {
+//        for (j, item) in row.enumerated() {
+        for j in 0 ..< dataSource.numberOfColumns {
+            let item = dataSource.itemFor(row: i, column: j)
             let paddingNeeded = columnWidths[j] - item.count
             let padding = repeatElement(" ", count:
                 paddingNeeded).joined(separator: "")
@@ -34,12 +49,68 @@ func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
     
     }
 
-    let data = [
-        ["Joe", "30", "6"],
-        ["Karen", "40", "18"],
-        ["Fred", "50", "20"],
-    
-    ]
-    
-    printTable(data, withColumnLabels: "Employee Name", "Age", "Years of Experience")
+//    let data = [
+//        ["Joe", "30", "6"],
+//        ["Karen", "40", "18"],
+//        ["Fred", "50", "20"],
+//
+//    ]
+//
+//    printTable(data, withColumnLabels: "Employee Name", "Age", "Years of Experience")
+//
 
+struct Person {
+    let name: String
+    let age: Int
+    let yearsOfExperience: Int
+}
+
+struct Department: TabularDataSource {
+    let name: String
+    var people = [Person]()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    mutating func add(_ person: Person) {
+        people.append(person)
+    }
+    
+    var numberOfRows: Int {
+        return people.count
+    }
+    
+    var numberOfColumns: Int {
+        return 3
+    }
+    
+    func label(forColumn column: Int) -> String {
+        switch column {
+        case 0: return "Employee Name"
+        case 1: return "Age"
+        case 2: return "Years of Experience"
+        default: fatalError("Invialid column!")
+                
+        }
+    }
+    
+    func itemFor(row: Int, column: Int) -> String {
+        let person = people[row]
+        switch column {
+        case 0: return person.name
+        case 1: return String(person.age)
+        case 2: return String(person.yearsOfExperience)
+            
+        default: fatalError("Invalid column!")
+        }
+    
+    }
+}
+
+var department = Department(name: "engineering")
+department.add(Person(name: "Joe", age: 30, yearsOfExperience: 6))
+department.add(Person(name: "Karen", age: 40, yearsOfExperience: 18))
+department.add(Person(name: "Fred", age: 50, yearsOfExperience: 20))
+
+printTable(department)
